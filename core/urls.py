58948d2560747d0a5dba29f437ee9ae1ee0c32f4
Juzeka <rafaelgomesalmeida@hotmail.com>
2023-01-25 16:58:48 -0300

    refatorando urls

--- core/urls.py
+++ core/urls.py
@@ -1,50 +1,57 @@
 from django.urls import path, include
-from rest_framework.routers import SimpleRouter
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-from authentication.views import (
-    ContentTypeViewSet,
-    GroupViewSet,
-    PermissionViewSet,
-    UserViewSet
-)
-from agendamentos.views import AgendamentoViewSet
-from clientes.views import ClienteViewSet
-from configuracoes.views import ConfiguracaoViewSet
-from disponibilidades.views import DisponibilidadeViewSet
-from funcionarios.views import FuncionarioViewSet
-from horarios.views import HorarioViewSet
-from servicos.views import ServicoViewSet
-
-
-routers = SimpleRouter()
-
-routers.register(r'auth/users', UserViewSet, basename='users')
-routers.register(r'auth/permissions', PermissionViewSet, basename='permissions')
-routers.register(r'auth/groups', GroupViewSet, basename='groups')
-routers.register(r'auth/apps', ContentTypeViewSet, basename='apps')
-routers.register(r'schedulings', AgendamentoViewSet, basename='agendamentos')
-routers.register(r'mgt/clients', ClienteViewSet, basename='clientes')
-routers.register(r'mgt/settings', ConfiguracaoViewSet, basename='configuracoes')
-routers.register(r'mgt/availabilities', DisponibilidadeViewSet, basename='disponibilidades')
-routers.register(r'mgt/officials', FuncionarioViewSet, basename='funcionarios')
-routers.register(r'mgt/schedules', HorarioViewSet, basename='horarios')
-routers.register(r'mgt/services', ServicoViewSet, basename='servicos')
-
-
 from django.contrib import admin
+
 
 urlpatterns = [
     path('admin', admin.site.urls), #temporario
-    path('api/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/', include(routers.urls), name='api'),
+    path(
+        'api/auth/',
+        include('authentication.urls'),
+        name='auth'
+    ),
+    path(
+        'api/mgt/clients/',
+        include('clientes.urls'),
+        name='clients'
+    ),
+    path(
+        'api/mgt/schedulings/',
+        include('agendamentos.urls'),
+        name='schedulings'
+    ),
+    path(
+        'api/mgt/settings/',
+        include('configuracoes.urls'),
+        name='settings'
+    ),
+    path(
+        'api/mgt/availabilities/',
+        include('disponibilidades.urls'),
+        name='availabilities'
+    ),
+    path(
+        'api/mgt/officials/',
+        include('funcionarios.urls'),
+        name='officials'
+    ),
+    path(
+        'api/mgt/schedules/',
+        include('horarios.urls'),
+        name='schedules'
+    ),
+    path(
+        'api/mgt/services/',
+        include('horarios.urls'),
+        name='services'
+    ),
 ]
 
 urlpatterns += [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('doc/api/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path(
+        'doc/api/',
+        SpectacularSwaggerView.as_view(url_name='schema'),
+        name='swagger-ui'
+    ),
 ]
